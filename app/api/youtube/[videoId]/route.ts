56ba7e63c093037cc1a70a5cce5caee07745d7e9
ari-12-2000 <arikum12000@gmail.com>
2025-10-20 20:29:28 +0530

    Added a missing import

--- app/api/youtube/[videoId]/route.ts
+++ app/api/youtube/[videoId]/route.ts
@@ -1,6 +1,7 @@
 import { fetchVideo } from "@/lib/youtube";
+import { NextRequest } from "next/server";
 
-export async function GET({params}:{params:Promise<{videoId:string}>}){
+export async function GET(req:NextRequest, {params}:{params:Promise<{videoId:string}>}){
     const {videoId}= await params;
     return fetchVideo(videoId);
 }
