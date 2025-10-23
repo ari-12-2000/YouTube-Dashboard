9736424a3e83fb5502bc5cbb21fd3cc61c8bec03
ari-12-2000 <arikum12000@gmail.com>
2025-10-23 13:54:35 +0530

    removed callback url

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       {!session ? (
         <button
           onClick={() =>
-            signIn("google", { callbackUrl: "/" })
+            signIn("google")
           }
           className="bg-blue-500 text-white px-4 py-2 rounded"
         >
